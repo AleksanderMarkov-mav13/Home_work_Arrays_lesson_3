8cb59369e3fd2a93e0e3ee30f77976883051e3b4
Alexander Markov <markov_av_77@mail.ru>
2024-09-15 12:40:21 +0400

    Initial commit

---
+++ 3_1_Banquet_table.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    cout << "\033[2J\033[1;1H";
+    int tableware[2][6] = {{4, 3, 3, 3, 3, 3}, {4, 3, 3, 3, 3, 3}};
+    int plates[2][6] = {{3, 2, 2, 2, 2, 2}, {3, 2, 2, 2, 2, 2}};
+    int chaires[2][6] = {{1, 1, 1, 1, 1, 1}, {1, 1, 1, 1, 1, 1}};
+    ;
+}
