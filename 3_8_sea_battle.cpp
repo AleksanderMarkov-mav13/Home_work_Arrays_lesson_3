1849dfdfaf4493a30b6bfc1411941ad3368d466f
Alexander Markov <markov_av_77@mail.ru>
2024-09-22 12:42:25 +0400

    3_8 Initial commit

---
+++ 3_8_sea_battle.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "\033[2J\033[1;1H";
+    cout << "You can play sea_battle with this programme\n";
+    int field_one[10][10];
+    int field_two[10][10];
+}
